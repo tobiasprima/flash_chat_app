4ba63b68c1c3c62a06e7fd7dc3935f876060dc08
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-09 14:47:43 +0700

    Add Animations

--- lib/screens/welcome_screend.dart
+++ lib/screens/welcome_screend.dart
@@ -8,7 +8,33 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController? controller;
+  Animation? animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    animation = CurvedAnimation(parent: controller!, curve: Curves.easeIn);
+
+    controller!.forward();
+
+    animation!.addStatusListener((status) {
+      print(status);
+    });
+
+    controller!.addListener(() {
+      print(controller!.value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +47,12 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: animation!.value,
+                  ),
                 ),
                 Text(
                   'Flash Chat',

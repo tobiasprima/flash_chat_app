add15ddd218845aea7facb99cbd9c155b16b8f33
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-09 17:36:37 +0700

    Refactor the Code

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,11 @@ void main() => runApp(FlashChat());
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData.dark().copyWith(
-          textTheme: TextTheme(
-            bodyText1: TextStyle(color: Colors.black54),
-          ),
-        ),
-        initialRoute: WelcomeScreen.id,
-        routes: {
-          WelcomeScreen.id: (context) => WelcomeScreen(),
-          ChatScreen.id: (context) => ChatScreen(),
-          LoginScreen.id: (context) => LoginScreen(),
-          RegistrationScreen.id: (context) => RegistrationScreen(),
-        });
+    return MaterialApp(initialRoute: WelcomeScreen.id, routes: {
+      WelcomeScreen.id: (context) => WelcomeScreen(),
+      ChatScreen.id: (context) => ChatScreen(),
+      LoginScreen.id: (context) => LoginScreen(),
+      RegistrationScreen.id: (context) => RegistrationScreen(),
+    });
   }
 }

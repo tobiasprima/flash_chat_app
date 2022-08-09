e30bb14c26dd242540f895a8b84562706615a7ed
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-09 15:24:56 +0700

    import package:animated_text_kit

--- lib/screens/welcome_screend.dart
+++ lib/screens/welcome_screend.dart
@@ -1,6 +1,7 @@
 import 'package:flash_chat_app/screens/login_screen.dart';
 import 'package:flash_chat_app/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';

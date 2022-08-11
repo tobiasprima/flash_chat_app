416154f2b94e2e426e6dd9b7c6b5bf78a4ec0460
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-11 21:32:58 +0700

    Beautify the looks

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -35,13 +35,6 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  // void getMessages() async {
-  //   final messages = await _firestore.collection('messages').get();
-  //   for (var message in messages.docs) {
-  //     print(message.data());
-  //   }
-  // }
-
   void messagesStream() async {
     await for (var snapshot in _firestore.collection('messages').snapshots()) {
       for (var message in snapshot.docs) {}
